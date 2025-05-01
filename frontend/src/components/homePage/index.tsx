ebd5d6abb58867fb8264dd80c7f12a22161de7cf
kaikeMKY <kaikemassaokamidayamashita@gmail.com>
2025-05-01 19:40:45 -0300

    style: new backgroundAnimation

--- frontend/src/components/homePage/index.tsx
+++ frontend/src/components/homePage/index.tsx
@@ -33,7 +33,7 @@ const Home = () => {
 			<h1>Genius Game</h1>
 			<div className="GameModes">
 				<div className='divButtonHome'>
-					<ButtonHomePageLink buttontext="Solo Game"to="/soloGame"id="Solo-Game"/>
+					<ButtonHomePageLink buttontext="Solo Game"to="/soloGame"id="Solo-Game"/> 
 					<ButtonHomePageLink buttontext="1 vs 1" to="/" id="One-vs-One" />
 				</div>
 				<div className='divButtonHome'>
